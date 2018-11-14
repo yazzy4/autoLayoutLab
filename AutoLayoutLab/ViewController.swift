f2b9c9d6baf639ca983394ad91355f0df8011a8e
yazzy4 <yazzy4@gmail.com>
2018-11-14 14:23:35 -0500

    first commit

---
+++ AutoLayoutLab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutLab
+//
+//  Created by Yaz Burrell on 11/14/18.
+//  Copyright © 2018 Yaz Burrell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("doin git")
+    }
+
+
+}
+
